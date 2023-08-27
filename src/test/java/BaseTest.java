a79cd8b369ace7e2615cda96fb3e0107c92367c3
natkim86 <142276432+natkim86@users.noreply.github.com>
2023-08-27 14:06:00 -0500

    homework18 changes

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -15,11 +17,12 @@ public class BaseTest {
     public WebDriver driver = null;
     public String url = "https://qa.koel.app/";
 
+    WebDriverWait wait;
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
-
 
 
     @BeforeMethod
@@ -27,28 +30,31 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
-        driver =new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver = new ChromeDriver(options);
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(100));
     }
 
     protected void clickSubmit() {
-        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
         submit.click();
     }
 
     protected void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
         passwordField.clear();
         passwordField.sendKeys("AxKrdBnS");
     }
 
     protected void provideEmail(String email) {
 
-        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
         emailField.clear();
         emailField.sendKeys(email);
     }
+
     protected void navigateToPage() {
         driver.get(url);
     }
--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,34 +1,37 @@
 import org.openqa.selenium.By;
-        import org.openqa.selenium.WebElement;
-        import org.testng.Assert;
-        import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 
-public class Homework18 extends BaseTest{
-
+public class Homework18 extends BaseTest {
 
 
     @Test
- public void playSong(){
-    navigateToPage();
-    provideEmail("natalia.kim01@testpro.io");
-    providePassword("AxKrdBnS");
-    clickSubmit();
-    clickPlay();
-    validateSong();
+    public void playSong() {
+        navigateToPage();
+        provideEmail("natalia.kim01@testpro.io");
+        providePassword("AxKrdBnS");
+        clickSubmit();
+        clickPlay();
+        validateSong();
     }
 
-   private void clickPlay() {
-       WebElement playNextSong = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
-     WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+    private void clickPlay() {
+        clickOnElement(By.cssSelector("div.side.player-controls"));
+        clickOnElement (By.cssSelector("[data-testid='play-next-btn']"));
+        clickOnElement(By.cssSelector("[data-testid='play-btn']"));
 
-      playNextSong.click();
-      playButton.click();
 
+    }
+    void clickOnElement(By locator){
+        WebElement el = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        el.click();
     }
 
     public void validateSong() {
-        WebElement soundBar = driver.findElement(By.cssSelector("[data-testid='sound-bar-play']"));
+        WebElement soundBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-testid='sound-bar-play']")));
         Assert.assertTrue(soundBar.isDisplayed());
     }
 
